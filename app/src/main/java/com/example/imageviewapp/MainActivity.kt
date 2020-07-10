b389006a6efe3a59a3bf928b20f375feaca56a7c
Lucian <lucian.popescu187@gmail.com>
2020-07-10 18:09:52 +0300

    Replaced findViewById with DataBinding

--- app/src/main/java/com/example/imageviewapp/MainActivity.kt
+++ app/src/main/java/com/example/imageviewapp/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.imageviewapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.example.imageviewapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private lateinit var binding: ActivityMainBinding
 
-        val fetchPhotoButton = findViewById<Button>(R.id.button)
-        fetchPhotoButton.setOnClickListener { openPhotoActivity() }
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener { openPhotoActivity() }
 
     }
 
--- app/src/main/java/com/example/imageviewapp/PrintPhotoActivity.kt
+++ app/src/main/java/com/example/imageviewapp/PrintPhotoActivity.kt
@@ -6,30 +6,39 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.imageviewapp.databinding.ActivityPrintPhotoBinding
 
 class PrintPhotoActivity : AppCompatActivity() {
 
     private val galleryFetchRequest = 0
     private var photoView: ImageView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_print_photo)
+    private lateinit var binding: ActivityPrintPhotoBinding
 
-        photoView = findViewById(R.id.imageView)
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPrintPhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        photoView = binding.imageView
 
         val galleryIntent = Intent(Intent.ACTION_PICK,
             android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         startActivityForResult(galleryIntent, galleryFetchRequest)
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+
         super.onActivityResult(requestCode, resultCode, intent)
 
         if (resultCode == Activity.RESULT_OK && requestCode == galleryFetchRequest) {
             val fetchedImage = intent?.data as Uri
             photoView?.setImageURI(fetchedImage)
         }
+
     }
 
 }
